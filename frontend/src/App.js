eff1360e2caa33d80010484cbb890d253153e30d
James <jburke1153088@gmail.com>
2024-06-08 16:32:36 -0400

    adding investment tracker to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ExpenseTracker from './components/ExpenseTracker/ExpenseTracker';
+import InvetmentTracker from './components/InvestmentTracker/InvestmentTracker';
 import Layout from './components/Layout/Layout';
+import Dashboard from './components/Dashboard/dashboard';
+import InvestmentTracker from './components/InvestmentTracker/InvestmentTracker';
 
 const App = () => {
     return (
         <Router>
             <Layout>
                 <Routes>
-                    <Route path="/" element={<ExpenseTracker />} />
-                    {/* Add more routes here if needed */}
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/expense-tracker" element={<ExpenseTracker />} />
+                    <Route path="/investment-tracker" element={<InvestmentTracker />} />
                 </Routes>
             </Layout>
         </Router>
