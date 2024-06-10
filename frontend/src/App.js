43d9333a04e2f73e5b0e040499d6ff2494b8892b
James <jburke1153088@gmail.com>
2024-06-09 20:42:27 -0400

    app.js update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
@@ -5,16 +6,17 @@ import ExpenseTracker from './components/ExpenseTracker/ExpenseTracker';
 import InvestmentTracker from './components/InvestmentTracker/InvestmentTracker';
 
 const App = () => {
-    return (
-        <Router>
-            <Layout>
-                <Routes>
-                    <Route path="/expense-tracker" element={<ExpenseTracker />} />
-                    <Route path="/investment-tracker" element={<InvestmentTracker />} />
-                </Routes>
-            </Layout>
-        </Router>
-    );
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/expense-tracker" element={<ExpenseTracker />} />
+          <Route path="/investment-tracker" element={<InvestmentTracker />} />
+          <Route path="/" element={<h1 className="text-center text-4xl mt-8">Welcome to the Finance Dashboard</h1>} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 };
 
 export default App;
